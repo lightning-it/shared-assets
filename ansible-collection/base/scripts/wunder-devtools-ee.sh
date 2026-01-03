6889f540788ff64e737657de64d17080a7fb1653
Rene Osorio <ro@l-it.io>
2026-01-02 21:54:08 -0500

    chore: update image registry from GHCR to Quay.io in scripts and renovate configuration

--- ansible-collection/base/scripts/wunder-devtools-ee.sh
+++ ansible-collection/base/scripts/wunder-devtools-ee.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-IMAGE="${WUNDER_DEVTOOLS_EE_IMAGE:-ghcr.io/lightning-it/wunder-devtools-ee:v1.1.7}"
+IMAGE="${WUNDER_DEVTOOLS_EE_IMAGE:-quay.io/l-it/ee-wunder-ansible-ubi9:v1.1.4}"
 CONTAINER_HOME="${CONTAINER_HOME:-/tmp/wunder}"
 HOST_HOME_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/wunder-devtools-ee/home"
 
