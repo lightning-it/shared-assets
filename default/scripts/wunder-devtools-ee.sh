9a7ad9a0a6d1ef686da49c1358e5a2799654b62b
Rene Osorio <ro@l-it.io>
2026-01-02 22:43:17 -0500

    chore: update image tag for wunder-devtools-ee to v1.1.8

--- default/scripts/wunder-devtools-ee.sh
+++ default/scripts/wunder-devtools-ee.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-IMAGE="quay.io/l-it/wunder-devtools-ee:v1.1.7"
+IMAGE="quay.io/l-it/wunder-devtools-ee:v1.1.8"
 CONTAINER_HOME="${CONTAINER_HOME:-/tmp/wunder}"
 HOST_HOME_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/wunder-devtools-ee/home"
 
